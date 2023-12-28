23bcc6ddd83526d6df6084f4ef6cf4088c84f176
tranduy <haphanbaominh9674@gmail.com>
2023-12-29 00:18:45 +0700

    test CI/CD

--- exe201_backend/settings.py
+++ exe201_backend/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'exe201_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-  
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
