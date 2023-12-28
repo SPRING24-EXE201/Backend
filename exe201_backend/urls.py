82bb932f8e498d5f39dbb4636ae198eba7c8c145
tranduy <haphanbaominh9674@gmail.com>
2023-12-29 00:48:22 +0700

    test ci/cd

--- exe201_backend/urls.py
+++ exe201_backend/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cost-version/', include('cabinet.web_api.urls.cost_version_url')),
 ]
