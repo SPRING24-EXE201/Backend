70a4c01ff4782f70d4a522a9f03691171685243c
minhncn2310 <minh27122002pro@gmail.com>
2023-12-29 16:55:37 +0700

    CRUD CabinetType, Controller, Cabinet

--- cabinet/admin.py
+++ cabinet/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
-from cabinet.models import CostVersion
+from cabinet.models import CostVersion, Cabinet, CabinetType, Controller
 
 
 # Register your models here.
 @admin.register(CostVersion)
 class CostVersionAdmin(admin.ModelAdmin):
     list_display = ('version', 'cost', 'from_hour', 'to_hour')
+
+
+@admin.register(Cabinet)
+class CabinetAdmin(admin.ModelAdmin):
+    list_display = (
+        'controller_id', 'cabinetType_id', 'description', 'start_using_date', 'height', 'width', 'depth', 'status',
+        'image_link', 'virtual_cabinet_id')
+
+
+@admin.register(CabinetType)
+class CabinetTypeAdmin(admin.ModelAdmin):
+    list_display = ('type', 'description', 'status', 'image_link', 'cost_per_unit')
+
+
+@admin.register(Controller)
+class ControllerAdmin(admin.ModelAdmin):
+    list_display = ('location_id', 'name', 'kafka_id', 'topic', 'status')
--- cabinet/root_urls.py
+++ cabinet/root_urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 
 cabinet_root_urls = [
     path('cost-version/', include('cabinet.web_api.urls.cost_version_url')),
+    path('cabinet/', include('cabinet.web_api.urls.cabinet_url')),
+    path('cabinet-type/', include('cabinet.web_api.urls.cabinet_type_url')),
+    path('controller/', include('cabinet.web_api.urls.controller_url')),
 ]
