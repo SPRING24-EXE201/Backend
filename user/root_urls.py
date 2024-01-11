760633caf0410457efdf127afeb22c62c69dc205
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-11 09:56:21 +0700

    045 - add login api

--- user/root_urls.py
+++ user/root_urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('register/', include('user.web_api.urls.register_url'))
+    path('register/', include('user.web_api.urls.register_url')),
+    path('login/', include('user.web_api.urls.login_url'))
 ]
