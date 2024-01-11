64eba169df873313e9d976b13fac119bd7cfe183
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-11 09:55:22 +0700

    045 - change color for mail template

--- exe201_backend/common/utils.py
+++ exe201_backend/common/utils.py
@@ -14,11 +14,11 @@ class Utils:
         html_body = f"""<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
           <div style="margin:50px auto;width:70%;padding:20px 0">
             <div style="border-bottom:1px solid #eee">
-              <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">iBox</a>
+              <a href="" style="font-size:1.4em;color: #1D9D6C;text-decoration:none;font-weight:600">iBox</a>
             </div>
             <p style="font-size:1.1em">Xin chào,</p>
-            <p>Cảm ơn bạn đã tin dùng iBox. Hãy sử dụng OTP sau để hoàn thành quá trình {action} của bạn. OTP của bạn có hiệu lực trong {SystemConstants.otp_timeout / 60} phút</p>
-            <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{otp_code}</h2>
+            <p>Cảm ơn bạn đã tin dùng iBox. Hãy sử dụng OTP sau để hoàn thành quá trình {action} của bạn. OTP của bạn có hiệu lực trong {'%.0f' % (SystemConstants.otp_timeout / 60)} phút</p>
+            <h2 style="background: #1D9D6C;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{otp_code}</h2>
             <p style="font-size:0.9em;">Trân trọng,<br />iBox</p>
             <hr style="border:none;border-top:1px solid #eee" />
             <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
