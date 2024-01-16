66c65d3b4ca7773b87df029bb4e2873b589b6d0b
minhncn2310 <minh27122002pro@gmail.com>
2024-01-16 14:59:54 +0700

    #59 - Get Cabinet Details by cabinet_id

--- cabinet/web_api/serializers/cabinet_serializer.py
+++ cabinet/web_api/serializers/cabinet_serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from cabinet.models import Cabinet
+from cabinet.models import Cabinet, Cell
+from order.models import OrderDetail
+from datetime import datetime, timedelta
 
 
 class CabinetSerializer(serializers.ModelSerializer):
@@ -7,3 +9,23 @@ class CabinetSerializer(serializers.ModelSerializer):
         model = Cabinet
         fields = ['id', 'controller_id', 'cabinetType_id', 'description', 'start_using_date', 'height', 'width',
                   'depth', 'status', 'image_link', 'virtual_cabinet_id']
+
+
+class CabinetDetailsSerializer(serializers.ModelSerializer):
+    location_name = serializers.CharField(source='controller_id.location_id.location_name', read_only=True)
+    location_detail = serializers.CharField(source='controller_id.location_id.location_detail', read_only=True)
+    empty_cell = serializers.SerializerMethodField()
+    cabinet_type = serializers.CharField(source='cabinetType_id.type', read_only=True)
+
+    class Meta:
+        model = Cabinet
+        fields = ['location_name', 'location_detail', 'width', 'height', 'depth', 'empty_cell', 'cabinet_type']
+
+    def get_empty_cell(self, obj):
+        now = datetime.now()
+        count_user_is_null = Cell.objects.filter(cabinet_id=obj.id, user_id__isnull=True).count()
+        cells = OrderDetail.objects.extra(where=["time_start >= %s OR time_end <= %s"],
+                                          params=[now + timedelta(minutes=30), now]).count()
+        empty_cells_count = count_user_is_null + cells
+        return empty_cells_count
+        
--- cabinet/web_api/views/cabinet_view.py
+++ cabinet/web_api/views/cabinet_view.py
@@ -1,8 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import Http404
 
 from cabinet.models import Cabinet
-from cabinet.web_api.serializers.cabinet_serializer import CabinetSerializer
+from cabinet.web_api.serializers.cabinet_serializer import CabinetSerializer, CabinetDetailsSerializer
 
 
 @api_view(['GET'])
@@ -16,7 +17,25 @@ def get_cabinet(request):
     except Cabinet.DoesNotExist:
         pass
 
-    data = CabinetSerializer(cabinet, many=True).data
+    data = CabinetDetailsSerializer(cabinet, many=True).data
+
+    return Response({
+        'success': True,
+        'data': data,
+    })
+
+@api_view(['GET'])
+def get_cabinet_by_id(request, cabinet_id):
+    """
+    Get cabinet by id
+    """
+    cabinet = []
+    try:
+        cabinet = Cabinet.objects.get(id=cabinet_id)
+    except Cabinet.DoesNotExist:
+        raise Http404("Not found")
+
+    data = CabinetDetailsSerializer(cabinet).data
 
     return Response({
         'success': True,
