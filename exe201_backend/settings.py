fa32b42945d259f4fafc4bd9bc501b9c9ddcb509
hoangdapoet <huynhnhatthienhoang2002@gmail.com>
2024-01-17 11:14:23 +0700

    038 - Pull request again, add try except finally for CRUD Order

--- exe201_backend/settings.py
+++ exe201_backend/settings.py
@@ -94,7 +94,8 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
 
 
