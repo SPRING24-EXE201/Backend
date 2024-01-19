e5b5bd1bb0f9652df0e1947585897ae51f1fddc3
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-19 17:02:18 +0700

    060 - fix hash code cell unique, fix logic map cost version on campaign

--- cabinet/models.py
+++ cabinet/models.py
@@ -41,14 +41,14 @@ class CostVersion(models.Model):
 
 
 class Campaign(models.Model):
-    cost_version_id = models.ForeignKey(CostVersion, on_delete=models.CASCADE)
+    cost_version = models.CharField(max_length=100, null=False, blank=False, default='UNUSABLE')
     time_start = models.DateTimeField(null=True, blank=True, default=None)
     time_end = models.DateTimeField(null=True, blank=True, default=None)
     status = models.BooleanField()
     description = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.cost_version_id} - {self.status}"
+        return f"{self.cost_version} - {self.status}"
 
 
 class Cabinet(models.Model):
@@ -77,7 +77,7 @@ class Cell(models.Model):
     cabinet = models.ForeignKey(Cabinet, on_delete=models.CASCADE)
     user_id = models.IntegerField(null=True)
     status = models.PositiveSmallIntegerField()
-    hash_code = models.CharField(max_length=100)
+    hash_code = models.CharField(max_length=100, unique=True)
     cell_index = models.PositiveSmallIntegerField()
     width = models.FloatField()
     height = models.FloatField()
