9f2afb66805686e352d49b051f821f3628d413e1
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-19 12:49:58 +0700

    060 - implement get all empty cells based on specified cabinet

--- cabinet/web_api/serializers/cabinet_serializer.py
+++ cabinet/web_api/serializers/cabinet_serializer.py
@@ -1,5 +1,8 @@
+import datetime
+from datetime import timedelta
+
 from rest_framework import serializers
-from cabinet.models import Cabinet
+from cabinet.models import Cabinet, Cell
 
 
 class CabinetSerializer(serializers.ModelSerializer):
@@ -7,3 +10,22 @@ class CabinetSerializer(serializers.ModelSerializer):
         model = Cabinet
         fields = ['id', 'controller_id', 'cabinetType_id', 'description', 'start_using_date', 'height', 'width',
                   'depth', 'status', 'image_link', 'virtual_cabinet_id']
+
+
+class EmptyCellsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cell
+        fields = ['hash_code', 'cell_index', 'width', 'height', 'depth']
+
+
+class EmptyCellsRequestSerializer(serializers.Serializer):
+    cabinet_id = serializers.IntegerField(required=True)
+    time_start = serializers.DateTimeField(required=True)
+    time_end = serializers.DateTimeField(required=True)
+
+    def validate(self, data):
+        time_start = data['time_start']
+        time_end = data['time_end']
+        if time_start + timedelta(minutes=30) >= time_end:
+            raise serializers.ValidationError({'errorMessage': 'time range is not valid'})
+        return data
