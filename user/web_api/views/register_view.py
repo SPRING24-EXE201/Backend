c7e2e0118551e8dc177bbf7e06d7476cc982abdc
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-21 22:17:20 +0700

    077 - Refactor code

--- user/web_api/views/register_view.py
+++ user/web_api/views/register_view.py
@@ -8,23 +8,17 @@ from user.web_api.serializers.register_serializer import RegisterSerializer
 
 
 @extend_schema(
-    request=RegisterSerializer
+    request={'application/json': RegisterSerializer}
 )
 @api_view(['POST'])
 def register(request):
     serializer = RegisterSerializer(data=request.data)
     if serializer.is_valid():
         email = serializer.data.get('email')
-        try:
-            User.objects.create_user(email=email, password='')
-        except Exception as e:
-            return Response({
-                'Message': str(e)
-            }, status=400)
+        User.objects.create_user(email=email, password='')
         rs = Utils.send_otp(serializer.data.get('email'), 'đăng ký')
         if rs:
             return Response({
                 'Message': 'Đã gửi mail',
             })
-        return Response(status=400)
     return Response(status=400)
