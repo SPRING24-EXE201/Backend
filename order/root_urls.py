284b3bf31d7a1eec15348b7cc93ebfd1df0eecab
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-21 22:24:00 +0700

    077 - Refactor Code

--- order/root_urls.py
+++ order/root_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('order.web_api.urls.order_url')),
-    path('order_detail/', include('order.web_api.urls.order_detail_url'))
+    path('order/', include('order.web_api.urls.order_url')),
+    path('order_detail/', include('order.web_api.urls.order_detail_url')),
+    path('valid-time/', include('order.web_api.urls.valid_order_time_url'))
 ]
