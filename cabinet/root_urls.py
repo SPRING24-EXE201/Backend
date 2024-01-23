813a8e16c0d5189560b51053e49c34ce8bebf8ae
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-24 01:20:41 +0700

    094 - refactor URL

--- cabinet/root_urls.py
+++ cabinet/root_urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('cost-version/', include('cabinet.web_api.urls.cost_version_url')),
-    path('campaign/', include('cabinet.web_api.urls.campaign_url')),
-    path('campaign-cabinet/', include('cabinet.web_api.urls.campaign_cabinet_url')),
-    path('cells/', include('cabinet.web_api.urls.cell_url')),
-    path('cell-logs/', include('cabinet.web_api.urls.cell_log_url')),
-    path('cabinet-opening/', include('cabinet.web_api.urls.cabinet_opening_url')),
+    # path('cost-version/', include('cabinet.web_api.urls.cost_version_url')),
+    # path('campaign/', include('cabinet.web_api.urls.campaign_url')),
+    # path('campaign-cabinet/', include('cabinet.web_api.urls.campaign_cabinet_url')),
+    # path('cells/', include('cabinet.web_api.urls.cell_url')),
+    # path('cell-logs/', include('cabinet.web_api.urls.cell_log_url')),
+    path('cabinet/opening', include('cabinet.web_api.urls.cabinet_opening_url')),
     path('cabinet/', include('cabinet.web_api.urls.cabinet_url')),
-    path('cabinet-nearby/', include('cabinet.web_api.urls.cabinet_nearby_url'))
+    path('cabinet', include('cabinet.web_api.urls.cabinet_nearby_url'))
 ]
