909a70512a0141cf2c18abe1d966c8653f38a6f3
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-01-24 22:29:48 +0700

    common - add calculate total cost in a order detail util

--- exe201_backend/common/utils.py
+++ exe201_backend/common/utils.py
@@ -7,7 +7,7 @@ from django.core.cache import cache
 from django.core.mail import EmailMultiAlternatives
 from django.utils import timezone
 
-from cabinet.models import CostVersion, Cell
+from cabinet.models import CostVersion, Cell, CampaignCabinet
 from exe201_backend.common.constants import SystemConstants
 from order.models import OrderDetail
 
@@ -45,7 +45,7 @@ class Utils:
         return True
 
     @staticmethod
-    def calc_total_cost(campaign, order_time_start, order_time_end):
+    def calc_total_cost_in_campaign(campaign, order_time_start, order_time_end):
         """
         Calculate total cost in a range of campaign
         :param campaign:
@@ -75,6 +75,27 @@ class Utils:
             total_cost += (duration_in_version.total_seconds() / version.unit.total_seconds()) * version.cost
 
         return round(total_cost, 0)
+
+    @staticmethod
+    def calc_total_cost_in_order_detail(hash_code, time_start, time_end):
+        """
+        :param: cell_id, time_start, time_end
+        :return: total cost in order detail
+        :exception: Cell.DoesNotExist, CampaignCabinet.DoesNotExist, CostVersion.DoesNotExist
+        """
+        total_cost = 0
+        cell = Cell.objects.get(hash_code=hash_code)
+        campaign_cabinets = (CampaignCabinet.objects.filter(cabinet__id=cell.cabinet.id,
+                                                            campaign__status=True,
+                                                            campaign__time_end__gte=time_start,
+                                                            campaign__time_start__lte=time_end)
+                             .select_related('campaign').order_by('campaign__time_start'))
+        if not campaign_cabinets:
+            raise CampaignCabinet.DoesNotExist
+        campaigns = [campaign_cabinet.campaign for campaign_cabinet in campaign_cabinets]
+        for valid_campaign in campaigns:
+            total_cost += Utils.calc_total_cost_in_campaign(valid_campaign, time_start, time_end)
+        return total_cost
 
     @staticmethod
     def check_valid_cells(data):
