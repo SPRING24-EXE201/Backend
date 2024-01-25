1e9f21bb84d54ed18c1f07c78e0f31435a836115
Nhat Tan <phannhattan201447@gmail.com>
2024-01-25 21:44:05 +0700

    099 - update user information

--- user/web_api/urls/user_url.py
+++ user/web_api/urls/user_url.py
@@ -3,5 +3,5 @@ from django.urls import path
 from user.web_api.views import user_view
 
 urlpatterns = [
-    path('', user_view.get_user, name='get_user')
+    path('', user_view.update_user, name='update_user')
 ]
