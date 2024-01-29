5df397a15923208a34ed2c5c2f022d4aca0af049
tranduy <haphanbaominh9674@gmail.com>
2024-01-29 07:24:40 +0700

    add order url

--- order/root_urls.py
+++ order/root_urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    # path('/', include('order.web_api.urls.order_url')),
+    path('/', include('order.web_api.urls.order_url')),
     # path('/order_detail/', include('order.web_api.urls.order_detail_url')),
     path('valid-time', include('order.web_api.urls.valid_order_time_url')),
     path('purchase', include('order.web_api.urls.purchase_url'))
