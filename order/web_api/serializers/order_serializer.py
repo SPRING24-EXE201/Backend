4b04da09359b44154c424b2bcfcf586243954355
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-02-03 01:09:01 +0700

    111 - update get order detail by cell and time range

--- order/web_api/serializers/order_serializer.py
+++ order/web_api/serializers/order_serializer.py
@@ -3,6 +3,27 @@ from order.models import Order
 from order.models import OrderDetail
 
 
+class OrderCellRequestSerializer(serializers.Serializer):
+    page_size = serializers.IntegerField(required=False)
+    page = serializers.IntegerField(required=False)
+    cell_id = serializers.IntegerField(required=False)
+    start_date = serializers.DateTimeField(required=False)
+    end_date = serializers.DateTimeField(required=False)
+    is_desc = serializers.BooleanField(required=False)
+
+    def validate(self, data):
+        time_start = data.get('start_date')
+        time_end = data.get('end_date')
+        if time_start is None or time_end is None:
+            return data
+        is_valid_range = time_start <= time_end
+        if not is_valid_range:
+            raise serializers.ValidationError({
+                'message': 'Khoảng thời gian không hợp lệ'
+            })
+        return data
+
+
 class OrderSerializer(serializers.ModelSerializer):
     order_detail_id = serializers.IntegerField(source='id')
     cell_index = serializers.IntegerField(source='cell.cell_index')
