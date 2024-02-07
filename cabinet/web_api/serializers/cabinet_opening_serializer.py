0ed118660ee85783bed8946143b30baa13c68230
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-02-07 22:08:50 +0700

    117 - update api opening cabinet

--- cabinet/web_api/serializers/cabinet_opening_serializer.py
+++ cabinet/web_api/serializers/cabinet_opening_serializer.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
 
-class CabinetOpeningSerializerRequest(serializers.Serializer):
-    hash_code = serializers.CharField(max_length=100)
 
-class CabinetOpeningSerializerResponse(serializers.Serializer):
-    controller_id = serializers.IntegerField()
-    cabinet_id = serializers.IntegerField()
-    cell_index = serializers.IntegerField()
+class CabinetOpeningSerializerRequest(serializers.Serializer):
+    hash_code = serializers.UUIDField(required=True)
--- cabinet/web_api/views/cabinet_opening_view.py
+++ cabinet/web_api/views/cabinet_opening_view.py
@@ -1,43 +1,39 @@
-from rest_framework.decorators import api_view, permission_classes
-from django.http import JsonResponse
-from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from cabinet.models import Cell
-from django.core.exceptions import PermissionDenied
-from cabinet.web_api.serializers.cabinet_opening_serializer import CabinetOpeningSerializerRequest, CabinetOpeningSerializerResponse
-from exe201_backend import service_bus, settings
+from cabinet.web_api.serializers.cabinet_opening_serializer import CabinetOpeningSerializerRequest
+from exe201_backend.common.utils import Utils
 
 
 @extend_schema(
-    request = CabinetOpeningSerializerRequest
+    request=CabinetOpeningSerializerRequest
 )
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def get_cabinet_opening_view(request):
-    try:
-        serializers = CabinetOpeningSerializerRequest(data=request.data)
-        if serializers.is_valid():
-            hash_code = serializers.data.get('hash_code')
-            access_token = request.auth
-            user_id = access_token['user_id']
+    serializers = CabinetOpeningSerializerRequest(data=request.data)
+    data = {}
+    status_code = 200
+    if serializers.is_valid(raise_exception=True):
+        hash_code = serializers.data.get('hash_code')
+        access_token = request.auth
+        user_id = access_token['user_id']
+        try:
             cell = Cell.objects.get(hash_code=hash_code)
-
-            if user_id == cell.user:
-                controller = cell.cabinet.controller
-                json_data = {"controller_id": controller.id, "cabinet_id": cell.cabinet_id, "cell_index": cell.cell_index}
-                data = CabinetOpeningSerializerResponse(json_data, many = False ).data
-                service_bus.handler_message(str(json_data), settings.SERVICE_BUS_TOPIC_NAME)
+            user_own_cell = Utils.get_user_own_cell(hash_code)
+            if user_own_cell and user_own_cell.id == user_id:
+                Utils.send_command(hash_code)
+                data = 'Mở tủ thành công'
                 status_code = 200
             else:
-                raise PermissionDenied("Không có quyên truy cập")
-    except Cell.DoesNotExist:
-        data = {'message': "Ô tủ không tồn tại"}
-        status_code = 400
-    except PermissionDenied as e:
-        data = {'message': str(e)}
-        status_code = 403
-    except Exception as e:
-        data = {'message': "Lỗi hệ thống"}
-        status_code = 500
-    finally:
-        return JsonResponse(data, status=status_code)
+                data = 'Không có quyền mở tủ'
+                status_code = 403
+        except Cell.DoesNotExist:
+            data = 'Không tìm thấy ô tủ'
+            status_code = 400
+    return Response(status=status_code, data={
+        'message': data
+    })
