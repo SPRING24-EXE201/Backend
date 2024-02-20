00747b04e905bff76526ae2ce17cfe80f065fe62
tranduy <haphanbaominh9674@gmail.com>
2024-02-20 22:48:20 +0700

    hot fix

--- chartapp/urls.py
+++ chartapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
-    path('/purchase', staff_member_required(views.purchase), name='purchase'),
-    path('/box-event', staff_member_required(views.box_event), name='box_event'),
+    path('purchase', staff_member_required(views.purchase), name='purchase'),
+    path('box-event', staff_member_required(views.box_event), name='box_event'),
 ]
