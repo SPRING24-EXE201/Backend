795c13f355b01fc23306b14a458c9af8cf3395b3
Hùng Khanh <hungkhanh.lof@gmail.com>
2024-03-04 14:29:38 +0700

    fix payos_id

--- exe201_backend/common/custom_id.py
+++ exe201_backend/common/custom_id.py
@@ -28,8 +28,8 @@ def ward_custom_id():
 
 def payment_order_id():
     from order.models import Order
-    no = Order.objects.all().aggregate(Max('payment_order_id', default=40))
+    no = Order.objects.all().aggregate(Max('payment_order_id', default=70))
     if not no:
-        return 40
+        return 70
     else:
         return no.get('payment_order_id__max') + 1
